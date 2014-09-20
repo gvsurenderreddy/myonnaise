465d06eab1df39a8b49a16d66b844ffae1b7e2ee
Michael Ahn <msa.ahn@gmail.com>
2014-09-20 11:31:58 -0400

    Added tray icon for application.

--- src/qtcreator/myoenu/mainwindow.cpp
+++ src/qtcreator/myoenu/mainwindow.cpp
@@ -1,14 +1,50 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMenu>
+#include <QAction>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    createTrayIcon();
+
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void
+MainWindow::createTrayIcon()
+{
+    mTrayIcon = new QSystemTrayIcon(this);
+
+    QAction *onQuit = new QAction( "Exit", mTrayIcon );
+    connect( onQuit, SIGNAL(triggered()), this, SLOT(onTrayExit()) );
+
+    QAction *onShow = new QAction( "Do something", mTrayIcon );
+    connect( onShow, SIGNAL(triggered()), this, SLOT(onTrayShow()) );
+
+    QMenu *trayMenu = new QMenu;
+    trayMenu->addAction( onQuit );
+    trayMenu->addAction( onShow );
+
+    mTrayIcon->setContextMenu( trayMenu );
+
+    mTrayIcon->show();
+}
+
+void
+MainWindow::onTrayQuit()
+{
+
+}
+
+void
+MainWindow::onTrayShow()
+{
+
 }
--- src/qtcreator/myoenu/mainwindow.h
+++ src/qtcreator/myoenu/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSystemTrayIcon>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    void createTrayIcon();
+
 private:
     Ui::MainWindow *ui;
+
+    QSystemTrayIcon *mTrayIcon;
+
+public slots:
+    void onTrayQuit();
+    void onTrayShow();
+
 };
 
 #endif // MAINWINDOW_H
