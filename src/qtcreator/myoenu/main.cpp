f28626c520e4e25b3cdd7c5b1364fb5fffcad49b
Michael Ahn <msa.ahn@gmail.com>
2014-09-20 04:33:22 -0400

    Hid title bar and made application fullscreen.

--- src/qtcreator/myoenu/main.cpp
+++ src/qtcreator/myoenu/main.cpp
@@ -5,7 +5,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+
+    //Hide the title bar and show fullscreen
+    w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+    w.showFullScreen();
 
     return a.exec();
 }
