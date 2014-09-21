527f0e4a3e35f9843959dd535f9be2d9b60ab17e
KevXman <kevinxiao2@gmail.com>
2014-09-20 22:18:05 -0400

    Left right and fist functionality

--- src/lua/myoenu.lua
+++ src/lua/myoenu.lua
@@ -3,11 +3,16 @@ scriptId = 'com.misahn.Test'
 listenForSwipeDownIn = false
 listenForSwipeUpOut = false
 reset = false
+verticalMove = false
+horizontalMove = true
 
 totalDeltaY = 0
 lastPitch = 0
-minSwipeSpd = 0.05
-swipeDistance = 0.15
+totalDeltaX = 0
+lastYaw = 0
+minSwipeSpd = 0.07
+swipeDistance = 0.20
+currentPose = "none"
 
 function onSwipeDown()
 	myo.debug("SWIPE DOWN")
@@ -19,16 +24,36 @@ function onSwipeUp()
 	myo.keyboard("up_arrow", "press")
 end
 
+function onSwipeRight()
+	myo.debug("SWIPE RIGHT")
+	myo.keyboard("right_arrow", "press")
+end
+
+function onSwipeLeft()
+	myo.debug("SWIPE LEFT")
+	myo.keyboard("left_arrow", "press")
+end
+
+function onOpenMenu()
+	myo.debug("OPEN MENU")
+	myo.keyboard("right_shift", "press", "control", "alt", "shift")
+end
+
 function onPoseEdge(pose, edge)
 	if edge == "on" then
+		currentPose = pose
 		if pose == "fist" then
-			myo.debug("Myo data: Roll " .. myo.getRoll() .. " Yaw " .. myo.getYaw() .. " Pitch " .. myo.getPitch())
 			myo.vibrate("short")
+			myo.debug("Fist")
+			listenForSwipeDownIn = true
+			listenForSwipeUpOut = true
 		elseif pose == "waveOut" then
-			myo.debug("Swipe down started")
+			myo.vibrate("short")
+			myo.debug("Swipe down/left started")
 			listenForSwipeDownIn = true
 		elseif pose == "waveIn" then
-			myo.debug("Swipe up started")
+			myo.vibrate("short")
+			myo.debug("Swipe up/right started")
 			listenForSwipeUpOut = true
 		end
 	end
@@ -39,35 +64,98 @@ function onPeriodic()
 	local currentPitch = myo.getPitch()
 	local deltaY = currentPitch - lastPitch
 	lastPitch = currentPitch
+	-- Get the difference in yaw, which is the horizontal angular movement
+	local currentYaw = myo.getYaw()
+	local deltaX = currentYaw - lastYaw
+	lastYaw = currentYaw
+	local ThresholdX = (math.abs(deltaX) < minSwipeSpd)
+	local ThresholdY = (math.abs(deltaY) < minSwipeSpd)
 
 	-- We want the difference in pitch to be downwards, and be fast enough
-	if deltaY < 0  and math.abs(deltaY) > minSwipeSpd then
+	if deltaY < 0  and math.abs(deltaY) > minSwipeSpd and currentPose ~= "fist" then
 		totalDeltaY = totalDeltaY - deltaY
 		myo.debug("swiping down")
 		-- Check if we've achieved the total downwards movement to count as a swipe
-		-- We don't want multiple swipe events to trigger in a short period of time
-		-- Note that to trigger this, we need a thumbToPinkyEvent to have happened beforehand
+		-- We need a thumbToPinkyEvent to have happened beforehand
 		if listenForSwipeDownIn and math.abs(totalDeltaY) > swipeDistance then
 			myo.vibrate("short")
 			onSwipeDown()
 			totalDeltaY = 0
 			reset = true
+			verticalMove = true
 		end
-	elseif math.abs(deltaY) > minSwipeSpd then
+	-- We want the difference in pitch to be upwards, and be fast enough
+	elseif math.abs(deltaY) > minSwipeSpd and currentPose ~= "fist" then
 		totalDeltaY = totalDeltaY - deltaY
 		myo.debug("swiping up")
-		-- Check if we've achieved the total downwards movement to count as a swipe
-		-- We don't want multiple swipe events to trigger in a short period of time
-		-- Note that to trigger this, we need a thumbToPinkyEvent to have happened beforehand
+		-- Check if we've achieved the total upwards movement to count as a swipe
+		-- We need a thumbToPinkyEvent to have happened beforehand
 		if listenForSwipeUpOut and math.abs(totalDeltaY) > swipeDistance then
 			myo.vibrate("short")
 			onSwipeUp()
 			totalDeltaY = 0
 			reset = true
+			verticalMove = true
 		end
-	else
+	end
+
+	-- We want the difference in yaw to be to the left, and be fast enough
+	if deltaX > 0  and math.abs(deltaX) > minSwipeSpd then
+		totalDeltaX = totalDeltaX - deltaX
+		myo.debug("swiping left")
+		-- Check if we've achieved the total movement to the left to count as a swipe
+		-- We need a thumbToPinkyEvent to have happened beforehand
+		if listenForSwipeDownIn and math.abs(totalDeltaX) > swipeDistance then
+			myo.vibrate("short")
+			if currentPose == "fist" then
+				onOpenMenu()
+				horizontalMove = false
+				verticalMove = false
+				totalDeltaX = 0
+				totalDeltaY = 0
+				reset=false
+				listenForSwipeUpOut = false
+				listenForSwipeDownIn = false
+			else
+				onSwipeLeft()
+			end
+			totalDeltaX = 0
+			reset = true
+			horizontalMove = true
+		end
+	-- We want the difference in pitch to be to the right, and be fast enough
+	elseif math.abs(deltaX) > minSwipeSpd then
+		totalDeltaX = totalDeltaX - deltaX
+		myo.debug("swiping right")
+		-- Check if we've achieved the total movement to the right to count as a swipe
+		-- We need a thumbToPinkyEvent to have happened beforehand
+		if listenForSwipeUpOut and math.abs(totalDeltaX) > swipeDistance then
+			myo.vibrate("short")
+			if currentPose == "fist" then
+				onOpenMenu()
+				horizontalMove = false
+				verticalMove = false
+				totalDeltaX = 0
+				totalDeltaY = 0
+				reset=false
+				listenForSwipeUpOut = false
+				listenForSwipeDownIn = false
+			else
+				onSwipeRight()
+			end
+			totalDeltaX = 0
+			reset = true
+			horizontalMove = true
+		end
+	end
+	-- check if the swipe has ended and reset the variables
+	if (horizontalMove and ThresholdX) or (verticalMove and ThresholdY) then
 		-- Reset our distance counter
+		horizontalMove = false
+		verticalMove = false
+		totalDeltaX = 0
 		totalDeltaY = 0
+		-- Reset booleans if already performed action
 		if reset then
 			reset=false
 			listenForSwipeUpOut = false
